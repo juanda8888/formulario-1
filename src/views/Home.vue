82d05d91e59d264b0ee1994b70387acbdf4ef79e
JUAN DAVID PARRA PATIÑO <jparrapa@externos.bankia.com>
2021-05-18 18:00:29 +0200

    finalizado la construcción del formulario

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,113 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <form @submit.prevent="procesarFormulario">
+    <input 
+      type="text"
+      class="form-control my-2"
+      placeholder="Ingrese nombre"
+      v-model.trim="tarea.nombre"
+    >
+    
+    <div class="form-check form-check-inline">
+      <input 
+        class="form-check-input" 
+        type="checkbox" 
+        id="inlineCheckbox1" 
+        value="javascript"
+        v-model="tarea.categorias"
+      >
+      <label class="form-check-label" for="inlineCheckbox1">JavaScript</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input 
+        class="form-check-input" 
+        type="checkbox" 
+        id="inlineCheckbox2" 
+        value="nodejs"
+        v-model="tarea.categorias"
+      >
+      <label class="form-check-label" for="inlineCheckbox2">Node.js</label>
+    </div>
+    <div class="mt-2">
+      <div class="form-check form-check-inline">
+        <input 
+          class="form-check-input" 
+          type="radio"
+          id="inlineRadio1"
+          name="estado"
+          value="urgente"
+          v-model="tarea.estado">
+        <label class="form-check-label" for="inlineRadio1">Urgente</label>
+      </div>
+      <div class="form-check form-check-inline">
+        <input 
+          class="form-check-input" 
+          type="radio"
+          id="inlineRadio2"
+          name="estado"
+          value="relax"
+          v-model="tarea.estado">
+        <label class="form-check-label" for="inlineRadio2">Relax</label>
+      </div>
+    </div>
+
+    <div class="mt-2">
+      <input 
+        type="number"
+        class="form-control"
+        v-model.number="tarea.numero"
+      >
+    </div>
+
+    <button 
+      class="btn btn-dark mt-2 btn-block"
+      type="submit"
+      :disabled="bloquear"
+    >Procesar
+    </button>
+  </form>
+  <hr>
+  <p>{{tarea}}</p>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+  },
+  data() {
+    return {
+      tarea: {
+        nombre: '',
+        categorias: [],
+        estado: '',
+        numero: 0
+      }
+    }
+  },
+  methods: {
+    procesarFormulario(){
+      console.log(this.tarea)
+      if(this.tarea.nombre.trim() === "") {
+        console.log('campo vacio');
+
+        return
+      }
+      console.log('no esta vacio')
+      //enviamos datos
+      this.tarea= {
+        nombre: '',
+        categorias: [],
+        estado: '',
+        numero: 0
+      }
+    }
+  },
+  computed: {
+    bloquear(){
+      return this.tarea.nombre.trim() === "" ? true : false;
+    }
   }
 }
 </script>
